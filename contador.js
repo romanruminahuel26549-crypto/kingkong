988e4ed5c3f5f02b584344d8df005d3de631c70d
willhuberselim12-ship-it <geronimotomascampos@gmail.com>
2025-09-08 21:48:12 -0300

    contadores

---
+++ contador.js
@@ -0,0 +1,14 @@
+function kingKong(n){
+    for(let i=1;i<=n;i++){
+        if(i%3===0 && i%5===0){
+            console.log('kingKong');
+        }else if(i%3===0){
+            console.log('King');
+        }else if(i%5===0){
+            console.log('Kong')
+        }else{
+            console.log(i)
+        }
+    }
+}
+kingKong(100);
