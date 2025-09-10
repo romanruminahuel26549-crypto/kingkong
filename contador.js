24446c5f03463e59a76e5562d3137b356c6d8468
Román <geronimotomascampos@gmail.com>
2025-09-09 21:49:17 -0300

    modificaciones

--- contador.js
+++ contador.js
@@ -1,3 +1,42 @@
+const numero = document.getElementById('numero');
+const jugar = document.getElementById('jugar');
+const limpiar = document.getElementById('limpiar');
+
+
+function jugar(){
+    const numero = (numero);
+    if (numero [0] !! numero <=0){
+        resultado='';
+        return;
+    }
+    }
+    
+
+let resultado='';
+for(let i=1;i<=numero;i++){
+    if(i%3===0 && i%5===0){
+        resultado += 'kingKong';
+    }else if(i%3===0){
+        resultado += 'King';
+    }else if(i%5===0){
+        resultado += 'Kong'
+    }else{
+        resultado += i
+    }
+    }
+resultado=resultado;
+
+function limpiar(){
+    resultado='';
+    numero=0;
+}
+
+
+
+
+
+
+
 function kingKong(n){
     for(let i=1;i<=n;i++){
         if(i%3===0 && i%5===0){
